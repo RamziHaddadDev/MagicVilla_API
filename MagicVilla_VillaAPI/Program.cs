b2f1341ab36c3cacc25382b88dc076538d8e00e3
Ramzi <haddad.ramzi.dev@gmail.com>
2023-04-21 00:10:42 +0100

    AutoMapping

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,11 +1,22 @@
+using AutoMapper;
+using MagicVilla_VillaAPI;
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers(option =>
+//Configuration de la base de Donnée
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
 {
-	option.ReturnHttpNotAcceptable = true;
-}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+builder.Services.AddControllers().AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
