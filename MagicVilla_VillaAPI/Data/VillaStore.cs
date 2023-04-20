89a3c9fdeb1e6073ce88ba791e02cec838b7b90d
Ramzi <haddad.ramzi.dev@gmail.com>
2023-04-20 03:04:07 +0100

    CRUD Functionality

---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,14 @@
+﻿using MagicVilla_VillaAPI.Models.Dto;
+
+namespace MagicVilla_VillaAPI.Data
+{
+	public static class VillaStore
+	{
+		public static List<VillaDTO> villaList = new()
+		{
+				new VillaDTO {Id=1,Name="Pool View",Occupancy=100,Sqft=4},
+				new VillaDTO {Id=2,Name="Beach View",Occupancy=300,Sqft=3}
+			};
+	}
+}
+
